4dd95056a1ea6bccb2772d0d278a62063586e64d
Sean Wells <sean.wells.sc@gmail.com>
2015-02-27 14:28:42 -0800

    chore(maintenance): updated gulp build
    Updated:
    
    - Removed unused watch task

--- gulp/config.js
+++ gulp/config.js
@@ -15,8 +15,7 @@ var path = require('path'),
 module.exports = {
 	port: '8080',
 	root: path.resolve('./'),
-	dist: './browser',
-	d: 'browser', //path.resolve('./browser'),
+	dist: './browser',	
 	name: pkg.name,
 	pkg: pkg
 };
--- gulp/tasks/connect.js
+++ gulp/tasks/connect.js
@@ -16,38 +16,15 @@ var gulp    = require('gulp'),
 /*
  * Task: connect
  *
- * Start the `Connect` HTTP server for viewing ./examples HTML samples.
- *
-gulp.task('connect', ['examples-watch'], function(){
-	var SEP = '\n============================================================\n',
-  	    app = connect()
-		//.use(connect.logger('dev'))
-		.use(connect.static(config.root));
-
-	http.createServer(app).listen(config.port);
-
-	setTimeout(function() {
-		gutil.log('\n' + SEP + 'The "examples" webserver is listening on port: '  +
-			gutil.colors.green(config.port) + '\n\n' +
-			'http://localhost:' + config.port + '/examples/tutorial/PATH_TO_EXAMPLE.html' +
-			SEP);
-	}, 1000);
-});
-*/
-
-console.log(config.d)
-/*
- * Task: connect
- *
  * Run a webserver for viewing ./examples HTML samples (with LiveReload)
  */
 gulp.task('connect', function() {
     connect.server({
-        root: [config.root +'/examples/', config.d],
+        root: [config.root +'/examples/', config.dist],
         port: config.port,
         livereload: true
     });
-    
+
     // Watch JS
     gulp.watch([config.name + '.js', './lib/**/*.js'], ['build']);
 
